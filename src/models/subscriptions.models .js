ce3725494b2fb74235a460d35dbff7c067cc70d2
IntelliEdtech <IntelliEdtech@gmail.com>
2024-10-07 19:01:04 +0530

    bug-fixed

--- src/models/subscriptions.models .js
+++ src/models/subscriptions.models .js
@@ -11,4 +11,4 @@ const subscriptionSchema = new Schema({
     }
 })
 
-export const SubscriptionSchema=mongoose.model("Subscription")
+export const SubscriptionSchema=mongoose.model("Subscription",subscriptionSchema)
